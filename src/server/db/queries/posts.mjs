d67666786013a749b3f2b288e0c9831a3c516ba3
abelkius <abelkius@gmail.com>
2018-12-24 00:21:06 +0100

    get single post

--- src/server/db/queries/posts.mjs
+++ src/server/db/queries/posts.mjs
@@ -1,7 +1,9 @@
 import knex from '../connection';
 
 export const getAllPosts = () => knex('posts').select('*');
+export const getSinglePost = (id) => knex('posts').select('*').where({ id: parseInt(id) });
 
 export default {
-    getAllPosts
+    getAllPosts,
+    getSinglePost
 };
--- src/server/routes/posts.mjs
+++ src/server/routes/posts.mjs
@@ -1,20 +1,33 @@
 import Router from 'koa-router';
 import queries from '../db/queries/posts';
 
-const basePath = '/api/posts';
+const BASE_PATH = '/api/posts';
 const router = new Router();
 
-router.get(basePath, async (ctx) => {
-    try {
-        const posts = await queries.getAllPosts();
-        ctx.body = {
-            status: 'success',
-            data: posts
-        };
-    }
-    catch (err) {
-        console.log(err);
-    }
-});
+router
+    .get(BASE_PATH, async (ctx) => {
+        try {
+            const posts = await queries.getAllPosts();
+            ctx.body = {
+                status: 'success',
+                data: posts
+            };
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })
+    .get(`${BASE_PATH}/:id`, async (ctx) => {
+        try {
+            const post = await queries.getSinglePost(ctx.params.id);
+            ctx.body = {
+                status: 'success',
+                data: post
+            };
+        }
+        catch (err) {
+            console.log(err);
+        }
+    });
 
 export default router;
